3cfd5c0711d7311cad402ff8928f08df8215778c
mohamedghaly140 <mohamedghaly140@gmail.com>
2020-08-03 23:09:23 +0200

    add menu component

--- src/components/ui/Header/Header.js
+++ src/components/ui/Header/Header.js
@@ -42,6 +42,19 @@ const useStyles = makeStyles(theme => ({
     marginLeft: '40px',
     marginRight: '25px',
   },
+  menu: {
+    backgroundColor: theme.palette.common.blue,
+    color: '#fff',
+  },
+  menuItem: {
+    ...theme.typography.tab,
+    paddingBottom: '10px',
+    paddingTop: '10px',
+    opacity: 0.7,
+    '&:hover': {
+      opacity: 1,
+    },
+  },
 }));
 
 const Header = () => {
@@ -54,6 +67,7 @@ const Header = () => {
   // Menu State
   const [anchorEl, setAnchorEl] = useState(null);
   const [open, setOpen] = useState(false);
+  const [selectedIndex, setSelectedIndex] = useState(0);
 
   const menuClickHandler = event => {
     setAnchorEl(event.currentTarget);
@@ -65,21 +79,75 @@ const Header = () => {
     setOpen(false);
   };
 
+  const menuItemClickHandler = (event, index) => {
+    setAnchorEl(null);
+    setOpen(false);
+    setSelectedIndex(index);
+  };
+
+  const menuOptions = [
+    { name: 'Services', link: '/services' },
+    { name: 'Custom Software Development', link: '/customsoftware' },
+    { name: 'Mobile App Development', link: '/mobileapps' },
+    { name: 'Website Development', link: '/websites' },
+  ];
+
   useEffect(() => {
-    if (window.location.pathname === '/' && value !== 0) {
-      setValue(0);
-    } else if (window.location.pathname === '/services' && value !== 1) {
-      setValue(1);
-    } else if (window.location.pathname === '/revolution' && value !== 2) {
-      setValue(2);
-    } else if (window.location.pathname === '/about' && value !== 3) {
-      setValue(3);
-    } else if (window.location.pathname === '/contact' && value !== 4) {
-      setValue(4);
-    } else if (window.location.pathname === '/estimate' && value !== 5) {
-      setValue(5);
+    switch (window.location.pathname) {
+      case '/':
+        if (value !== 0) {
+          setValue(0);
+        }
+        break;
+      case '/services':
+        if (value !== 1) {
+          setValue(1);
+          setSelectedIndex(0);
+        }
+        break;
+      case '/customsoftware':
+        if (value !== 1) {
+          setValue(1);
+          setSelectedIndex(1);
+        }
+        break;
+      case '/mobileapps':
+        if (value !== 1) {
+          setValue(1);
+          setSelectedIndex(2);
+        }
+        break;
+      case '/websites':
+        if (value !== 1) {
+          setValue(1);
+          setSelectedIndex(3);
+        }
+        break;
+      case '/revolution':
+        if (value !== 2) {
+          setValue(2);
+        }
+        break;
+      case '/about':
+        if (value !== 3) {
+          setValue(3);
+        }
+        break;
+      case '/contact':
+        if (value !== 4) {
+          setValue(4);
+        }
+        break;
+      case '/estimate':
+        if (value !== 5) {
+          setValue(5);
+        }
+        break;
+
+      default:
+        break;
     }
-  }, []);
+  }, [value]);
 
   return (
     <Fragment>
@@ -89,8 +157,8 @@ const Header = () => {
             <Button
               to="/"
               disableRipple
-              onClick={() => setValue(0)}
               component={Link}
+              onClick={() => setValue(0)}
               className={classes.logoContainer}
             >
               <img src={logo} alt="company logo" className={classes.logo} />
@@ -109,10 +177,10 @@ const Header = () => {
               />
               <Tab
                 aria-owns={anchorEl ? 'simple-menu' : undefined}
+                aria-haspopup={anchorEl ? 'true' : undefined}
+                onMouseOver={event => menuClickHandler(event)}
                 className={classes.tab}
-                component={Link}
-                to="/services"
-                label="Servics"
+                label="Services"
               />
               <Tab
                 className={classes.tab}
@@ -142,6 +210,30 @@ const Header = () => {
             >
               free estimate
             </Button>
+            <Menu
+              id="simple-menu"
+              open={open}
+              anchorEl={anchorEl}
+              onClose={menuCloseHandler}
+              MenuListProps={{ onMouseLeave: menuCloseHandler }}
+              classes={{ paper: classes.menu }}
+              elevation={0}
+            >
+              {menuOptions.map((option, i) => (
+                <MenuItem
+                  component={Link}
+                  to={option.link}
+                  classes={{ root: classes.menuItem }}
+                  onClick={event => {
+                    menuItemClickHandler(event, i);
+                    setValue(1);
+                  }}
+                  selected={i === selectedIndex && value === 1}
+                >
+                  {option.name}
+                </MenuItem>
+              ))}
+            </Menu>
           </Toolbar>
         </AppBar>
       </ElevationScroll>
